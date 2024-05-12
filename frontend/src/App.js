9a5821c1c2a2c87995de91731e9064fd13a61c4a
Ruhamma <ruhammaashenafi202@gmail.com>
2024-05-12 10:20:54 +0300

    Add nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-// import "./App.css";
 /** @jsxImportSource @emotion/react */
+import "./App.css";
 import React from "react";
-import { css } from "@emotion/react";
+import { css,Global } from "@emotion/react";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import SongList from "./components/SongList";
 import Nav from "./components/Nav";
+
 function App() {
   return (
     <Provider store={store}>
+      <Global
+        styles={css`
+          .secondary-color {
+            color: grey !important;
+          }
+        `}
+      />
       <Nav />
       <SongList />
     </Provider>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import React, { useEffect, useState } from "react";
+import { css } from "@emotion/react";
+import DayNightToggle from "react-day-and-night-toggle";
+import { FaMusic } from "react-icons/fa6";
 
 const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const body = document.body;
+    const toggleTheme = () => {
+      body.classList.toggle("dark-theme");
+    };
+
+    toggleTheme();
+
+    return () => window.removeEventListener("storage", toggleTheme);
+  }, [isDarkMode]);
   return (
-    <div>
-      <h1>Music Library</h1>;
+    <div
+      css={css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5rem;
+        padding-left: 5rem;
+        padding-right: 5rem;
+        transition: background-color 0.3s ease-in-out;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          gap: 10px;
+          font-size: 1.5rem;
+        `}
+      >
+        <FaMusic />
+        <p
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          Music Library
+        </p>
+      </div>
+      <DayNightToggle
+        onChange={() => setIsDarkMode(!isDarkMode)}
+        checked={isDarkMode}
+        size={30}
+      />
     </div>
   );
 };
